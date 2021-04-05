48c0e9667a80c49562dfcf2347a53f453bc07bea
Rodrigo Alves <rodrigohernandesalves@gmail.com>
2021-04-04 22:13:06 -0300

    Finalizando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep() {
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/ " + cep + "/json/";
 
@@ -9,13 +10,23 @@ function consultaCep() {
         type: "GET",
         success: function(response) {
             console.log(response);
-            $("#logradouro").html(response.logradouro) // ajax 
-                //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
+            $("#logradouro").html(response.logradouro);
+            $("#uf").html(response.uf);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("cep" + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            //$("#logradouro").html(response.logradouro) // ajax 
+
         }
     })
 }
+$(function() {
+    $(".cep").hide(); // faz sumiu as divs que mostram as informacoes e so aparece em cima no .show quando feita a consulta
+    $(".barra-progresso").hide();
+});
+
+
 
 // requisicao ajax utiliando a biblioteca jquery
